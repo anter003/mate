ec914093cfef034422b336aa6f4972177f60ec5c
anteryy <ppme7626@gmail.com>
2025-10-13 01:16:12 +0900

    리포 이동

--- src/App.svelte
+++ src/App.svelte
@@ -94,8 +94,8 @@
 
 <div class="credit flex flex-col fixed bottom-15 right-1 p-3 rounded-md text-gray-50 bg-gray-800">
   <h1 class="text-2xl font-semibold">20704 김성훈</h1>
-  <a href="https://codeberg.org/anteryy/mate" class="underline">Codeberg Repo</a>
-  <img src="../public/good.png">
+  <a href="https://github.com/anter003/mate" class="underline">Github Repo</a>
+  <img src="../public/good.png" alt="야호" width="200px">
 </div>
 
 
