11190368c61242a200dcfa6987d0f5861c5bd657
anteryy <ppme7626@gmail.com>
2025-10-13 18:27:36 +0900

    배경 조정 추가

--- src/App.svelte
+++ src/App.svelte
@@ -16,6 +16,8 @@
   let bias = $state(0);
   let x_init = $state(0);
   let eta = $state(0.002);
+  let ce = $state(100);
+  let grad = $state(true);
 
   const width = window.innerWidth;
   let height = 500;
@@ -26,7 +28,7 @@
     }  
 
     p.draw = () => {
-      s.draw(p, width, height, eq, eq_diff, domain, zoom, bias, x_init, eta);
+      s.draw(p, width, height, eq, eq_diff, domain, zoom, bias, x_init, eta, grad, ce);
     }
   }
 
@@ -55,7 +57,7 @@
     </div>
     <div class="PT text-3xl flex items-center text-center justify-between">
       <span class="mr-3">y={eq.to_string()}</span>
-      <img src="arrow.png" width="30%" class="mr-3" alt="arrow">
+      <img src="arrow.png" class="mr-3 w-[20%]" alt="arrow">
       <span>y'={eq_diff.to_string()}</span>
     </div>
     <div class="flex flex-col">
@@ -65,7 +67,7 @@
       </div>
       <div class="flex justify-end items-center">
         <span class="mr-1">배수 : x{zoom}</span>
-        <input type="range" min="0.2" max="10"  step="0.2" bind:value={zoom}>
+        <input type="range" min="0.2" max="10" step="0.2" bind:value={zoom}>
       </div>
       <div class="flex justify-end items-center">
         <span class="mr-1">이동 : {bias}</span>
@@ -83,16 +85,34 @@
     <input type="range" min="-30" max="30" step="0.1" bind:value={x_init} class="w-100">
   </div>
   <div class="flex items-center">
-    <span class="mr-1 block w-45">학습률(η) : {eta}</span>
-    <input type="range" min="0.00001" max="0.01" step="0.00001" bind:value={eta}>
+    <span class="mr-1 block w-45">Step Size(η) : {eta}</span>
+    <input type="range" min="0.00001" max="0.01" step="0.00001" bind:value={eta} class="mr-3">
   </div>
+  <div class="flex items-center">
+    <span class="mr-1 block w-20">색상: {ce}</span>
+    <input type="range" min="20" max="400" bind:value={ce}>
+  </div>
+</div>
+
+<div class="fixed  right-0 top-[50%] p-2.5 rounded-l-md text-center text-lg text-gray-50 bg-gray-900 shadow-lg">
+  <fieldset class="flex flex-col">
+    <h2 class="text-xl font-semibold">배경</h2>
+    <div>
+      <span class="mr-2 hover:underline" title="도함수의 함숫값을 배경색으로 설정합니다. 붉은색이 양수, 초록색이 0, 보라색이 음수입니다.">도함수</span>
+      <input type="radio" name="grad" value={true} bind:group={grad} class="cursor-pointer">
+    </div>
+    <div>
+      <span class="mr-2 hover:underline" title="x값에 따른 경사하강법의 수렴값을 색으로 배경색색으로 설정합니다. 흰색은 발산하는 구간입니다.">수렴값</span>
+      <input type="radio" name="grad" value={false} bind:group={grad} class="cursor-pointer">
+    </div>
+  </fieldset>
 </div>
 
 <div class="hov fixed bottom-0 right-0 p-2.5 rounded-tl-md text-center text-3xl text-gray-50 bg-gray-900 shadow-lg hover:bg-gray-800 cursor-pointer">
   ◎
 </div>
 
-<div class="credit flex flex-col fixed bottom-15 right-1 p-3 rounded-md text-gray-50 bg-gray-800">
+<div class="credit flex flex-col fixed bottom-13 right-0 p-3 rounded-md text-gray-50 bg-gray-800">
   <h1 class="text-2xl font-semibold">20704 김성훈</h1>
   <a href="https://github.com/anter003/mate" class="underline">Github Repo</a>
   <img src="good.png" alt="야호" width="100px">
--- src/sketch.js
+++ src/sketch.js
@@ -5,16 +5,20 @@ export function setup(p, w, h) {
   p.background(255);
 }
 
-export function draw(p, w, h, eq, eq_diff, domain, mult, bias, x_i, eta) {
+export function draw(p, w, h, eq, eq_diff, domain, mult, bias, x_i, eta, grads, ce) {
     let fn = (i) => eq.calc(i);
     let fn_diff = (i) => eq_diff.calc(i);
     let xz = 2;
 
     p.clear();
 
-    grad(p,w,h, domain, mult, fn_diff, xz);
+    if (grads) {
+        grad(p,w,h, domain, mult, fn_diff, xz, ce);
+    } else {
+        bg_minima(p,w,h,domain,mult,fn_diff,eta,xz, ce);
+    }
     equation(p,w,h , fn, domain, mult, bias, xz);
-    draw_g(p, w, h, fn, fn_diff, domain, mult, bias, x_i, eta, xz);
+    draw_lines(p, w, h, fn, fn_diff, domain, mult, bias, x_i, eta, xz);
 }
 
 function equation(p, w, h, fn, domain, mult, bias, xz) {
@@ -33,13 +37,13 @@ function equation(p, w, h, fn, domain, mult, bias, xz) {
     p.pop();
 }
 
-function grad(p,w,h, domain, mult, fn , xz) {
+function grad(p,w,h, domain, mult, fn , xz, ce) {
     p.push();
     p.translate(w / 2, 0);
     p.beginShape();
     
     for (let i = -domain; i < domain; i+=0.01) {
-        let y = normalize(mult * fn(i / xz),3, 100);
+        let y = normalize(mult * fn(i / xz),3, ce || 100);
         p.stroke(p.color("hsl(" + Math.round(y) + ",70%, 40%)"));
         p.strokeWeight(3);
         p.line(i * w / 2 / domain, 0, i * w / 2 / domain ,h);
@@ -54,36 +58,49 @@ function normalize(x, e, c) {
     return 290/d    
 }
 
-function draw_g(p, w, h, fn, fn_diff, domain, mult, bias, x_i, eta, xz) {
-    let steps = get_steps(fn, fn_diff, x_i, eta, mult, bias, xz);
+function draw_lines(p, w, h, fn, fn_diff, domain, mult, bias, x_i, eta, xz) {
+    let steps = get_steps(fn_diff, x_i, eta, mult, xz);
 
     p.push();
     p.translate(w / 2, 0);
     p.beginShape();
 
     p.stroke(0);
-    p.strokeWeight(3);
+    p.strokeWeight(5);
     p.line(x_i * w / 2 / domain, 0, x_i * w / 2 / domain, h);
 
+    
     p.strokeWeight(1);
     for (let s of steps) {
-        p.stroke(s.i * 4 + 20)
+        let color = s.i * 4 + 20;
+        p.stroke(color);
         p.line(s.x * w / 2 / domain, 0, s.x * w / 2 / domain, h);
+  
+    }
+
+    p.stroke(0);
+    p.strokeWeight(3);
+    p.textSize(20);
+    p.textAlign(p.CENTER , p.CENTER);
+    for (let s of steps) {
+        let color = s.i * 4 + 20;
+        if (s.i == 10 || s.i == 20 || s.i == 30 || s.i == 40 || s.i == 50) {
+            p.fill(color);
+            p.text(s.i, s.x * w / 2 / domain, 10 * s.i + 70);
+        }
     }
 
     p.endShape();
     p.pop();
-
-
 }
 
-function get_steps(fn, fn_diff, x_i, eta, mult, bias, xz) {
+function get_steps(fn_diff, x_i, eta, mult, xz) {
     let res = [];
 
-    let x = x_i;
+    let x = x_i / xz;
     let d = fn_diff(x_i);
 
-    for (let i = 1; i < 50; i++) {
+    for (let i = 1; i < 51; i++) {
         d = fn_diff(x) * eta * mult;
         x -= d;
 
@@ -91,4 +108,20 @@ function get_steps(fn, fn_diff, x_i, eta, mult, bias, xz) {
     }
 
     return res;
+}
+
+function bg_minima(p,w,h, domain, mult, fn_diff, eta, xz, ce) {
+    p.push();
+    p.translate(w / 2, 0);
+    p.beginShape();
+    
+    for (let i = -domain; i < domain; i+=0.01) {
+        let y = normalize(get_steps(fn_diff, i, eta, mult, xz)[49].x, 3, ce/20);
+        p.stroke(p.color("hsl(" + Math.round(y) + ",70%, 40%)"));
+        p.strokeWeight(3);
+        p.line(i * w / 2 / domain, 0, i * w / 2 / domain ,h);
+    }
+
+    p.endShape();
+    p.pop();
 }
